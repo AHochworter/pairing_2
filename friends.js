9c20520f3f940836eb8c53466b54f42abd373950
Ann Hochworter <ann.hochworter@gmail.com>
2023-06-07 16:56:44 -0600

    Add cities to visit array

--- friends.js
+++ friends.js
@@ -6,4 +6,13 @@ var arrays = ["Fred", "Ginger", "Ben", "Alice"]
 
 // console.log(arrays[3]);
 
-console.log(arrays[0]);
+// console.log(arrays[0]);
+
+var visit = ["Paris", "Barcelona", "Bangkok", "San Diego", "Nashville"]
+
+visit.pop();
+console.log(visit);
+// removed last element
+
+
+
